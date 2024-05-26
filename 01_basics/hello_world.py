e37916b9a9206eefe319b3d1fb6b1dad1a884695
Akshit Bansal <desiithoughtss@gmail.com>
2024-05-26 06:23:38 +0530

    added an app

--- 01_basics/hello_world.py
+++ 01_basics/hello_world.py
@@ -1,6 +1,14 @@
-print("Hello World")
+# print("Hello World")
 
-def func(n):
-    print(n)
+# def func(n):
+#     print(n)
 
-func(9)
+# func(9)
+
+# x = ("masala", "lemon", "ginger")
+# y = enumerate(x)
+# print(y)
+
+# print(list(y))
+
+file = open("test.py", "w")
